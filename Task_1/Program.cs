3d56fa4397c12bc2b508929b90e84e3114e3d676
Anjal-khadka <69anjal@gmail.com>
2025-11-10 12:27:32 +0545

    add task one content to remote branch

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_1;
+
+public class Program
+{
+    public static void Main()
+    {
+        Opeartors operators = new Opeartors();
+        operators.OddEvenFinder(2);
+        operators.Add(1, 2);
+        operators.Subtract(50, 40);
+        operators.Multiply(10, 20);
+        operators.Divide(10, 20);
+    }
+}
